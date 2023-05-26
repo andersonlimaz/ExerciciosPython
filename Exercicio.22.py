02e7d08d6ffedf8f04e86dfe4ae0afe0068771de
Andersonlimaz <andersonlimaz@outlook.com>
2023-05-26 12:15:36 -0300

    Novos exercicio em python

---
+++ Exercicio.22.py
@@ -0,0 +1,5 @@
+#Crie um progtam quer leia o nome completo de uma pessoa e mostre:
+#1 O nome com todas minusculas 
+#2 O nomr com todas maiusculas
+#3 Quantas letras ao todo (sem considerar espaços)
+#4 quantas letras tem o primeiro nome. 
---
+++ Exercicio_26.py
@@ -0,0 +1,4 @@
+#faça um programa que leia uma frase pelo teclado e mostre:
+#Quantas vezes aparece a letra "A"
+#Em que posição ela aparece a primeiro vez 
+#Em que posição ela aparece a ultima vez 
