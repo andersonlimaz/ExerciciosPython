bb56741e4a672802ba5d6cfb03f40db6a7290030
Andersonlimaz <andersonlimaz@outlook.com>
2023-05-31 10:36:19 -0300

    31 5 23

--- Exercicio_26.py
+++ Exercicio_26.py
@@ -3,3 +3,7 @@
 #Em que posição ela aparece a primeiro vez 
 #Em que posição ela aparece a ultima vez 
 
+frase = str(input('Digite uma frase ? ')).upper().strip()
+print('A letra A aparece {} vezes na frase'.format(frase.count('A')))
+print('A letra a apareceu na posição {}'.format(frase.find('A')+1))
+print('A ultima letra A apareceu na posição {}'. format(frase.rfind('A')+1))
