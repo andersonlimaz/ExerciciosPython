72afdc95f21f44de554364bda085b17262a005e6
andersonlimaz <93233117+andersonlimaz@users.noreply.github.com>
2023-06-01 18:08:26 -0300

    final do primeiro mundo

--- Exercicio_33.py
+++ Exercicio_33.py
@@ -1,4 +1,17 @@
 #faça um programa que leia tres numeros e mostre qual é o maior e qual é o menor. 
-n1 = int(input('informe o primeiro número'))
-n2 = int(input('informe o segundo número'))
-n3 = int(input('informe o terceiro número'))
+n1 = int(input('informe o primeiro número: '))
+n2 = int(input('informe o segundo número: '))
+n3 = int(input('informe o terceiro número: '))
+
+menor = n1 
+if n2 < n1 and n2 < n3:
+    menor = n2
+if n3<n1 and n3 < n2:
+    menor = n3
+print('o menor valor digitado foi {}'.format(menor))
+maior = n1
+if n2 > n1 and n2 > n3:
+    maior = n2
+if n3>n1 and n3 > n2:
+    maior = n3
+print('O maior valor digitado foi {}'.format(maior))
--- Exercicio_34.py
+++ Exercicio_34.py
@@ -1,3 +1,9 @@
 #Escreva um programa que pergunte o salario de um funcionario e calcule o valor do seu aumento
 #para salario superiores a R$1.250,00, calcule um aumento de 10%.
 #para os inferiores ou iguais, o aumento é de 15%
+salario = float(input('Qual o salário do funcionario ? R$' ))
+if salario <= 1250:
+    ajuste = salario + (salario * 15 / 100)
+else:
+    ajuste = salario + (salario * 10 / 100 )
+print('Quem ganhava R${:.2f}, passa a ganhar R${:.2f}'.format(salario, ajuste))
