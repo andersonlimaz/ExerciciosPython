24b0c025a48b45af2f0618fb5d632b3c1597a8b6
Andersonlimaz <andersonlimaz@outlook.com>
2023-06-01 13:15:17 -0300

    commits 01 06 2023

--- Exercicio_28.py
+++ Exercicio_28.py
@@ -1,3 +1,27 @@
 #Escreva um programa que faça o computador "pensar" em um número inteiro entre 0 e 5 e peça para o ususario tentar descobrir qual o numero escolhido pelo computador
 # O programa deverá escrever na tela se o usuário venceu ou perdeu 
+# print('Vou pensar em um número entre 0 e 5. Tente adivinhar...')
 
+# n = int(input('Em que número eu pensei ?'))
+# if n == 0:
+#     print('Você acertou parabens')
+# else:
+#     n != 0
+#     print('Você errou, tente novamente! ')
+#tentativa 
+#---------------------------------------------------------------------------------------------------------
+#correção 
+from random import randint
+from time import sleep
+computador = randint(0, 5) # Sorteia o numero
+print('-===' * 10)
+print('Vou pensar em um numero entre 0 e 5, tente adivinhar...')
+print('-===' * 10)
+jogador = int(input('Em que número eu pensei ? '))
+print('PROCESSANDO...')
+sleep(3)
+if jogador == computador:
+    print('Parabéns você acertou, o numero que eu pensei foi o {}'.format(computador))
+else:
+    jogador != computador
+    print('GANHEI, Eu pensei no número {} e você digitou {}'.format(computador, jogador))
