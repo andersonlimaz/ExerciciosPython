3a2807b6baec55fa096702e685e2bfc878e1a2bc
Andersonlimaz <andersonlimaz@outlook.com>
2023-06-06 12:44:14 -0300

    exercicio 06/06

--- Exercicio_37.py
+++ Exercicio_37.py
@@ -2,3 +2,9 @@
 #1 para binario 
 #2 para octal
 #3 para hexadecimal 
+num = int(input('Digite um numero inteiro: '))
+print('''Escolha uma bases para conversão:
+[1] converter para BINÁRIO
+[2] converte para OCTAL
+[3] converter para HEXADECIMAL''')
+opção = int(input('Sua opção: '))
