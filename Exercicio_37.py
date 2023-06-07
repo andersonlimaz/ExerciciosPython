4b2ee31e9249c762125bd52f0edf73cbd5b7910f
Andersonlimaz <andersonlimaz@outlook.com>
2023-06-07 09:16:19 -0300

    exercicio dia 07 06

--- Exercicio_37.py
+++ Exercicio_37.py
@@ -8,3 +8,11 @@ print('''Escolha uma bases para conversão:
 [2] converte para OCTAL
 [3] converter para HEXADECIMAL''')
 opção = int(input('Sua opção: '))
+if opção == 1:
+    print('{} convertido para BINARIO é igual a {}'.format(num, bin(num)[2:]))
+elif opção == 2:
+    print('{} convertido para OCTAL é igual a {}'.format(num, oct(num)[2:]))
+elif opção ==3:
+    print('{}convertido para HEXADECIMAL é igual a {}'.format(num, hex(num)[2:]))
+else:
+    print('Opção inválida. Tente novamente')
