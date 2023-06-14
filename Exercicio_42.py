85276de557acc0177a8db45a4e4f1e2a34402452
andersonlimaz <93233117+andersonlimaz@users.noreply.github.com>
2023-06-14 18:18:07 -0300

    commit 14 em casa

--- Exercicio_42.py
+++ Exercicio_42.py
@@ -2,3 +2,20 @@
 #Equilaterio: Todos lados são igual 
 #Isósceles: dois lados igual 
 #Escaleno: todos os lados diferentes 
+print('-+' * 10 )
+print('Analisando triângulo')
+print('-+' * 10 )
+
+r1 = float(input('Primeiro segmento: '))
+r2 = float(input('Segundo segmento: '))
+r3 = float(input('Terceiro segmento: '))
+
+if r1 < r2 + r3 and r2 < r1 + r3 and r3 < r1 + r2:
+    print('Os segmentos acima PODEM FORMAR triângulo!')
+
+    if r1 == r2 == r3:
+        print('Triângulo Equilatero')
+    elif r1 == r2 or r1 == r3 or r2 == r3:
+        print('Triângulo Isósceles')
+    elif r1 != r2 != r3:
+        print('Triângulo Escaleno') 
