e89e9531dfaf75fc0d5e2288aec5000764d318c7
Andersonlimaz <andersonlimaz@outlook.com>
2023-06-15 12:52:20 -0300

    Comits exercicios resolvidos

--- Exercicio_49.py
+++ Exercicio_49.py
@@ -1,1 +1,13 @@
 #refaça o desafio 09, mostrando a tabuada de um número que o usuário, só que agora ultilizando um laço for. 
+cont = 0
+print('********Tabuada*********')
+num = int(input('Digite um número: '))
+for c in range(1, 11):
+    resul = num * c
+    cont += 1 #cont = cont + 1 
+    print('{} X {} = {}'.format(cont,num,resul))
+
+#correção 
+num = int(input('Digite um número para ver sua tabuada: '))
+for c in range(1,11):
+    print('{} X {:2} = {}'.format(c, num, num*c)) 
--- Exercicio_50.py
+++ Exercicio_50.py
@@ -1,2 +1,10 @@
 #desenvolva um programa que leia seis números inteiros e mostre a soma apenas daqueles que forem pares.
 #Se o valor digitado dor ímpar, desconsidere-o. 
+soma = 0
+cont = 0
+for c in range(1, 7):
+    num = int(input('Informe o {} valor: '.format(c)))
+    if num % 2 == 0:
+        soma += num
+        cont += 1
+print('Você informou {} números PARES e a soma foi {}'.format(cont,soma))
