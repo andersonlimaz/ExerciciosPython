488bd11d39508726684857c56ed0c3fd99555255
Andersonlimaz <andersonlimaz@outlook.com>
2023-06-15 11:21:43 -0300

    commit 15/06/2023

--- Exercicio_48.py
+++ Exercicio_48.py
@@ -1,2 +1,9 @@
 #faça um programa que calcule a soma entre todos os números ímpares que são múltiplos de três
 # e que se encontram no intervalo de 1 até 500.
+soma = 0
+cont = 0
+for c in range(1, 501, 2):
+    if c % 3 == 0:
+        cont = cont + 1 #Conta o número de interações. pode usar tbm += 
+        soma = soma + c #Acumula os valores solicitados pode usar tbm += 
+print('A soma de todos os {} valores solicitado é {}'.format(cont,soma))
