87c5275a25459a565fa089bc1652c48692592e03
Andersonlimaz <andersonlimaz@outlook.com>
2023-06-20 10:05:06 -0300

    commit exercicio

--- Exercicio_57.py
+++ Exercicio_57.py
@@ -2,5 +2,12 @@
 #CASO ESTEJA ERRADO, PEÇA A DIGITAÇÃO NOVAMENTE ATÉ TER UM VALOR CORRETO. 
 sexo = str(input('Qual o seu sexo [F/M]: ')).upper()
 while sexo != 'M' and sexo != 'F':
-    print('Por favor digite um [M/F]')
+    print('Dados inválidos. Por favor, informe seu sexo: [M/F] ')
     sexo = str(input('Qual o seu sexo [F/M]: ')).upper()
+
+#correção 
+  
+sexo = str(input('Informe seu sexo: [M/F] ')).strip().upper()[0]
+while sexo not in 'MmFf':
+    sexo = str(input('Dados invalidos. Por favor, infroem seu sexo: ')).strip().upper()[0]
+print('Sexo {} regigitrado com sucesso'. format(sexo))
