47309d695de476931a6a3ff87172e5c69e810c19
Andersonlimaz <andersonlimaz@outlook.com>
2023-06-22 10:25:22 -0300

    commit 22/06/2023

--- Exercicio_61.py
+++ Exercicio_61.py
@@ -1,1 +1,12 @@
 #REFAÇA O DESAFIO 51, LENDO O PRIMEIRO TERMO E A RAZÃO DE UM PA, MOSTRANDO OS 10 PRIMEIROS TERMOS DA PROGRESSÃO USANDO A ESTRUTURA WHILE. 
+print('Gerador de Pa')
+print('-=' * 10)
+primeiro = int(input('Primeiro termo: '))
+razao = int(input('Razão da PA: '))
+termo = primeiro 
+cont = 1 
+while cont <= 10:
+    print('{} -> '.format(termo), end='')
+    termo += razao
+    cont += 1
+print('FIM')
--- Exercicio_62.py
+++ Exercicio_62.py
@@ -1,1 +1,18 @@
 #MELHORE O DESAFIO 61, PERGUNTANDO PARA USUÁRIO SE ELE QUER MOSTRAR MAIS ALGUNS TERMOS. O PROGRAMA ENCERRA QUANDO ELE DISSER QUE QUER MOSTRAR OS TERMOS. 
+print('Gerador de Pa')
+print('-=' * 10)
+primeiro = int(input('Primeiro termo: '))
+razao = int(input('Razão da PA: '))
+termo = primeiro 
+cont = 1 
+total = 0
+mais = 10 # programa começa mostrando 10 termos  
+while mais != 0:
+    total = total + mais
+    while cont <= total:
+        print('{} -> '.format(termo), end='')
+        termo += razao
+        cont += 1
+    print('Pausa')
+    mais = int(input('Quantos termos você quer mostrar a mais? '))
+print('Progressão finalizada com {} termos mostrados'.format(total))
