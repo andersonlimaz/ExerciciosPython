296f5b146a0de860ac6f51d2d1c6aba99fd4093c
Andersonlimaz <andersonlimaz@outlook.com>
2023-06-26 12:59:31 -0300

    exercicios 26/06/2023

---
+++ Exercicio_67.py
@@ -0,0 +1,4 @@
+#faça um programa que mostre a tabuada de varios números, um de cada vez, para cada valor digitado pelo usuário. 
+#O programa será interrompido quando o número solicitado for negativo. 
+print ('*' * 20 , 'Tabuada', '*' * 20)
+valor = int(input('Quer ver a tabuada de qual valor? '))
