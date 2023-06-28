0ea5358f5c7589cfa282590ead1df61b099fb572
Andersonlimaz <andersonlimaz@outlook.com>
2023-06-28 12:44:42 -0300

    28/06/2023

--- Exercicio_66.py
+++ Exercicio_66.py
@@ -2,10 +2,21 @@
 # O programa só vai parar quando o usuário digitar o valor 999, que pe a condição de para. No final, mostre quantos números foram digitados 
 # e qual a soma entre ele (desconsiderando o flag). 
 
-n = s = 0
+# n = s = 0
+# while True:
+#     n = int(input('Digite um número: '))
+#     if n == 999:
+#         break
+#     s += n
+# print('A soma vale {}'.format(s))
+
+#correção 
+cont = soma = 0
 while True:
-    n = int(input('Digite um número: '))
-    if n == 999:
+    num = int(input('Digite um valor (999 para parar)'))
+    if num == 999:
         break
-    s += n
-print('A soma vale {}'.format(s))
+    cont += 1
+    soma += num 
+
+print(f'A soma dos {cont} valores foi {soma}')
--- Exercicio_67.py
+++ Exercicio_67.py
@@ -1,16 +1,28 @@
-#faça um programa que mostre a tabuada de varios números, um de cada vez, para cada valor digitado pelo usuário. 
-#O programa será interrompido quando o número solicitado for negativo. 
-print('*' *20)
-print('Tabuada')
-print('*'* 20)
+# #faça um programa que mostre a tabuada de varios números, um de cada vez, para cada valor digitado pelo usuário. 
+# #O programa será interrompido quando o número solicitado for negativo. 
+# print('*' *20)
+# print('Tabuada')
+# print('*'* 20)
+# while True:
+#     valor = int(input('Qual o Valor que deseja calcular? '))
+#     print('para sair digite um valor negativo!')
+#     print('='*20)
+#     if valor < 0:
+#         break
+#     i = 1
+#     while i <= 10:
+#         resul = i * valor
+#         print (f'{i} X {valor} = {resul}')
+#         i += 1
+# print('Programa encerrado!')
+
+#correção 
 while True:
-    valor = int(input('Qual o Valor que deseja calcular? '))
-    print('para sair digite um valor negativo!')
-    if valor < 0:
+    n = int(input('Quer ver a tabuada de qual valor? '))
+    if n <0:
         break
-    i = 1
-    while i <= 10:
-        resul = i * valor
-        print (f'{i} X {valor} = {resul}')
-        i += 1
-print('Programa encerrado!')
+    print('-' * 30)
+    for c in range(1,11):
+        print(f'{n} X {c} = {n*c}')
+    print('-' * 30)
+print('Programa tabuada encerrado. Volte sempre!')
