d7b8804c4d63991ff697cceb73bd09028e52411e
Andersonlimaz <andersonlimaz@outlook.com>
2023-06-28 12:44:30 -0300

    correção dos exercicios primeira parte

--- Exercicio_68.py
+++ Exercicio_68.py
@@ -1,25 +1,27 @@
-# Faça um programa que jogue par ou impar com o computador. O jogo só será interrompido quando o jogador perder,
-#mostrando o total de vitórias consecutivas que ele conquistou no final do jogo
-import random 
-print('0-' *20)
-print('Vamos jogar IMPAR OU PAR')
-print('0-' *20)
+# # Faça um programa que jogue par ou impar com o computador. O jogo só será interrompido quando o jogador perder,
+# #mostrando o total de vitórias consecutivas que ele conquistou no final do jogo
+# from random import randint
+# print('0-' *20)
+# print('Vamos jogar IMPAR OU PAR')
+# print('0-' *20)
 
-vitorias =0
-while True:
-    valor_jogador = int(input('Diga o valor? '))
-    escolha_jogador = str(input('Par ou IMPAR [P/I]: ')).strip().upper()
+# vitorias =0
+# while True:
+#     valor_jogador = int(input('Diga o valor? '))
+#     escolha_jogador = str(input('Par ou IMPAR [P/I]: ')).strip().upper()
 
-    valor_comp = random.randint(1,10)
-    soma = valor_jogador + valor_comp
+#     valor_comp = randint(1,10)
+#     soma = valor_jogador + valor_comp
 
-    print(f'Você jogou {valor_jogador} e o computador jogou {valor_comp} o valor da soma é {soma}', end=' ')
-    print('DEU PAR!' if soma % 2 == 0 else 'DEU ÍMPAR!')
+#     print(f'Você jogou {valor_jogador} e o computador jogou {valor_comp} o valor da soma é {soma}', end=' ')
+#     print('DEU PAR!' if soma % 2 == 0 else 'DEU ÍMPAR!')
 
-    if (escolha_jogador == 'P' and soma % 2 == 0) or (escolha_jogador == 'I' and soma % 2 != 0):
-        print('Você VENCEU! ')
-        vitorias += 1
-    else:
-        print('Você Perdeu!')
-        break
-print(f'GAME OVER! Você conquistou {vitorias} vitoria(s) consecutiva(s)')
+#     if (escolha_jogador == 'P' and soma % 2 == 0) or (escolha_jogador == 'I' and soma % 2 != 0):
+#         print('Você VENCEU! ')
+#         vitorias += 1
+#     else:
+#         print('Você Perdeu!')
+#         break
+# print(f'GAME OVER! Você conquistou {vitorias} vitoria(s) consecutiva(s)')
+
+#correção 
