69a981f0e82cba098281b1c17cdc76007be22af9
Andersonlimaz <andersonlimaz@outlook.com>
2023-06-28 13:25:41 -0300

    exercicio 28/06/2023

--- Exercicio_68.py
+++ Exercicio_68.py
@@ -25,3 +25,33 @@
 # print(f'GAME OVER! Você conquistou {vitorias} vitoria(s) consecutiva(s)')
 
 #correção 
+from random import randint
+
+v = 0
+while True:
+    jogador = int(input('Diga o valor: '))
+    computador = randint(1,10)
+    total = jogador + computador
+    tipo = ' '
+    while tipo not in ('PpIi'):
+        tipo = str(input('Par ou Ímpar: [P/I]')).strip().upper()[0]
+    print(f'Você jogou {jogador} e o computador {computador}. total de {total}', end='')
+    print('DEU PAR' if total %2 == 0 else 'DEU ÍMPAR')
+    if tipo == 'P':
+        if total % 2 == 0:
+            print('Você Venceu!')
+            v += 1
+        else:
+            print('Você PERDEU!')
+            break
+    elif tipo == 'I':
+        if total % 2 == 1:
+            print('Você Venceu!')
+            v += 1
+        else:
+            print('Você PERDEU!')
+            break
+        print('Vamos jogar novamente...')
+print(f'Game Over! Você venceu {v} vezes')
+    
+
