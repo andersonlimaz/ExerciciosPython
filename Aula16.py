1e11b2311fa42100119f53b76dd081ea99f8d29d
Andersonlimaz <andersonlimaz@outlook.com>
2023-06-30 11:56:10 -0300

    commit aula 16

---
+++ Aula16.py
@@ -0,0 +1,27 @@
+#() tupla
+#[] lista
+#{} dicionario
+
+
+# lanche = ('humburguer', 'Suco','Pizza', 'Pudim' )
+# print(lanche[2:])
+
+
+
+#tuplas são imutáveis
+# lanche[1] = 'refrigetante'
+# print(lanche[1])
+#erro ocorre porque estamos tentando atribui valores a tupla. 
+
+#primeira maneira de execução
+# lanche = ('humburguer', 'Suco','Pizza', 'Pudim' )
+# for comida in lanche:
+#     print(f'Eu vou comer {comida}')
+# print('Comi para caramba')
+
+#segunda maneira de execução, mostra a posição. 
+# lanche = ('humburguer', 'Suco','Pizza', 'Pudim', 'batata frita' )
+# for cont in range(0, len(lanche)):
+#     print(f'Eu vou comer {lanche[cont]}')
+# print('comi para caramba')
+
