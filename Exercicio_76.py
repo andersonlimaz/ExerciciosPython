138a2c0c19efd4da3ebe373c72e9df8140fb9654
Andersonlimaz <andersonlimaz@outlook.com>
2023-07-03 12:01:42 -0300

    exercicio 76

--- Exercicio_76.py
+++ Exercicio_76.py
@@ -1,3 +1,20 @@
 #CRIE UM PROGRAMA QUE TENHA UMA TUPLA ÚNICA COM NOMES DE PRODUTOS E SEUS RESPECTIVOS PREÇOS NA SEQUÊNCIA.
 #NO FINAL, MOSTRE UMA LISTAGEM DE PREÇOS, ORGANIZANDO OS DADOS EM FORMA TABULAR.
-listagem = ('pão', 1, 'Leite', 3.50,'Frango', 10.90)
+listagem = ('lapiz', 1.75,
+            'Borracha', 2,
+            'Caderno', 15.90,
+            'Estojo', 25,
+            'Transferidor', 4.20,
+            'Compasso', 20.32,
+            'Mochila', 199.20,
+            'Caneta', 1.90,
+            'Livro', 15.98)
+print('-'*40)
+print(f'{"LISTAGEM DE PREÇOS":^40}')
+print('-'*40)
+for pos in range(0, len(listagem)):
+    if pos % 2 == 0:
+        print(f'{listagem[pos]:-<30}',end='')
+    else:
+        print(f'R${listagem[pos]:>7.2f}')        
+print('-'*40)
