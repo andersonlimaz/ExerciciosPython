cccc038e5ed0764019e679b81ccf53b1f2c06449
Andersonlimaz <andersonlimaz@outlook.com>
2023-07-03 10:54:34 -0300

    commite exercicio 74

--- Exercicio_74.py
+++ Exercicio_74.py
@@ -1,2 +1,9 @@
 #CRIE UM PROGRAMA QUE VAI GERAR CINCO NÚMEROS ALEATÓRIOS E COLOCAR EM UMA TUPLA. 
 #DEPOIS DISSO, MOSTRE A LISTAGEM DE NÚMEROS GERADOS E TAMBÉM INDIQUE O MENOR E O MAIOR VALOR QUE ESTÃO NA TUPLA. 
+from random import randint
+num = (randint(1,10), randint(1,10), randint(1,10), randint(1,10), randint(1,10),)
+print('Os valores sorteados foram: ',end='')
+for n in num:
+    print(f'{n}', end='')
+print(f'\nO maior valor sorteado foi {max(num)}')  
+print(f'O menor valor soreteado foi {min(num)}') 
