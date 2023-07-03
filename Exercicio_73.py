418a345e57a6e177540220e365a70a4aaa3d69f1
Andersonlimaz <andersonlimaz@outlook.com>
2023-07-03 10:32:23 -0300

    exercicio 73

--- Exercicio_73.py
+++ Exercicio_73.py
@@ -4,3 +4,16 @@
 #B-OS 4 ÚLTIMOS COLOCADOS DA TABELA
 #C-UM LISTA COM OS TIMES EM ORDEM ALFABÉTICA
 #D-EM QUE POSIÇÃO NA TABELA ESTÁ O TIME DA CHAPECOENSE. 
+times = ('Corinthians','Internacional','Fluminense','Flamengo','Athletico-PRA','tlético-MG','Fortaleza',
+                 'América-MG','Botafogo','Santos','Goiás','Red Bull', 'Bragantino','Coritiba','Cuiabá','Ceara',
+                 'Chapecoense','Avaí','Palmeiras','São Paulo')
+print('*'*30)
+print(f'Lista de times Brasileirão: {times}')
+print('*'*30)
+print(f'Os cinto primeiros colocados {times[0:5]}')
+print('*'*30)
+print(f'Os ultimos times são {times[16:]}')
+print('*'*30)
+print(f'Times em ordem alfabetica: {sorted(times)}')
+print('*'*30)
+print(f'O Chapecoense está na {times.index("Chapecoense")+1}ª posição')
