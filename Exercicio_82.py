41aa48c230c135bf203a7c7eee2836b481bdbdd4
Andersonlimaz <andersonlimaz@outlook.com>
2023-07-10 10:19:35 -0300

    exercicio 82 e 83

--- Exercicio_82.py
+++ Exercicio_82.py
@@ -1,3 +1,37 @@
 #Crie um programa que vai ler vários números e colocar em uma lista. 
 #Depois disso, crie duas listas extras que vão conter apenas os valores pares e os valores impares digitados, respectivamente
-#Ao final, mostre o conteúdo das três listas geradas. 
+# #Ao final, mostre o conteúdo das três listas geradas. 
+# valores = []
+# pares = []
+# impar = []
+# while True:
+#     valores.append(int(input('Digite um valor: ')))
+#     if valores 2 % == 0:
+#         pares.append(valores)
+#     else:
+#         impar.append(valores)
+#     resp = str(input('deseja sair : [S/N]'))
+#     if resp in 'Ss':
+#         break
+# print(f'A lista completa é {valores}')
+# print(f'A lista de pares é {pares}')
+# print(f'A lista de impares é {impar}')
+    
+num = list ()
+pares = list()
+impares = list()
+while True:
+    num.append(int(input('Digite um número: ')))
+    resp = str(input('Quer continuar: [S/N]'))
+    if resp in 'Nn':
+        break
+print(num)
+for i, v in enumerate(num):
+    if v % 2 == 0:
+        pares.append(v)
+    elif v % 2 == 1:
+        impares.append(v)
+print('*' *30)
+print(f'A lista completa {num}')
+print(f'A lista de pares é {pares}')
+print(f'A lista de impares é {impares}')
