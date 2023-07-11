01e87c3727cd88043cebb385d6f439fb515ecb2a
Andersonlimaz <andersonlimaz@outlook.com>
2023-07-11 12:28:47 -0300

    exercicio 85

--- Exercicio_85.py
+++ Exercicio_85.py
@@ -1,5 +1,21 @@
 #Crie um programa onde usuario possa digitar 7 valores númericos e cadastre-os em uma lista única
 #que mantenha separados os valores pares e imparaes. No final, mostre os valores pares e impares em ordem crescente
-lista = list()
-for i in range(1,7):
-    lista.append(int(input('')))
+lista = []
+pares = []
+impares = []
+
+for _ in range(7):
+    numero = int(input('Digite um valor numérico: '))
+    lista.append(numero)
+
+    if numero % 2 == 0:
+        pares.append(numero)
+    else:
+        impares.append(numero)
+
+pares.sort()
+impares.sort()
+
+print(f'Lista completa: {lista}')
+print(f'Números pares: {pares}')
+print(f'Números ímpares: {impares}')
