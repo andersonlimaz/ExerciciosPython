6b789689b7d3bb8cb00b5eb231d978bb6ef36ee8
Andersonlimaz <andersonlimaz@outlook.com>
2023-07-12 12:35:52 -0300

    commit 12/07

--- Exercicio_86.py
+++ Exercicio_86.py
@@ -1,2 +1,6 @@
 #crie um programa que crie matriz de dimensão 3x3
 #e preencha com valores lidos pelo teclado
+matriz = [[],[],[],[],[],[],[],[],[]]
+for i in range(0,9):
+    matriz[i].append(int(input(f'Digite o {i} valor:')))
+print(f'Matriz {matriz}')
