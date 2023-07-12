4b31a197537b2cb945fa843c325b9e8674c9ef98
Andersonlimaz <andersonlimaz@outlook.com>
2023-07-12 12:58:07 -0300

    exercicio 86 pronto

--- Exercicio_86.py
+++ Exercicio_86.py
@@ -1,6 +1,16 @@
 #crie um programa que crie matriz de dimensão 3x3
-#e preencha com valores lidos pelo teclado
-matriz = [[],[],[],[],[],[],[],[],[]]
-for i in range(0,9):
-    matriz[i].append(int(input(f'Digite o {i} valor:')))
-print(f'Matriz {matriz}')
+# #e preencha com valores lidos pelo teclado
+# matriz = [[],[],[],[],[],[],[],[],[]]
+# for i in range(0,9):
+#     matriz[i].append(int(input(f'Digite o {i} valor:')))
+# print(f'Matriz {matriz}')
+
+matriz = [[0, 0, 0],[0, 0, 0],[0, 0, 0]]
+for l in range(0,3):
+    for c in range(0,3):
+        matriz[l][c] = int(input(f'Digite um valor para [{l}, {c}]: '))
+print('-*'*90)
+for l in range(0, 3):
+    for c in range(0,3):
+        print(f'[{matriz[l][c]:^5}]', end='')
+    print()
