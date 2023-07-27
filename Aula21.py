6a31c1d8b611ccea8535b6161b5a68bf80a80c9f
andersonlimaz <93233117+andersonlimaz@users.noreply.github.com>
2023-07-26 22:11:17 -0300

    exercicios atulizados

--- Aula21.py
+++ Aula21.py
@@ -23,13 +23,41 @@
 # Escopo de variaveis
 # =======================
 
-def teste():
-    x= 8
-    print(f'Na função teste, n vale {n}')
-    print(f'Na função teste, x vale {x}')
+# def teste():
+#     x= 8
+#     print(f'Na função teste, n vale {n}')
+#     print(f'Na função teste, x vale {x}')
 
-#Programa Principal 
-n = 2
-print(f'No programa principal, n vale {n}')
-teste()
-print(f'No programa principal, x vale {x}')
+# #Programa Principal 
+# n = 2
+# print(f'No programa principal, n vale {n}')
+# teste()
+# print(f'No programa principal, x vale {x}')
+# ========================================
+# parte pratica da aula
+
+# def fatorial(num =1 ):
+#     f = 1
+#     for c in range(num, 0, -1):
+#         f*=c
+#     return f
+
+# n= int(input('Digite um número: '))
+# print(f'O fatorial de {n} é igual a {fatorial(n)}') 
+
+# Outro exemplo 
+
+
+# def fatorial(num =1 ):
+#     f = 1
+#     for c in range(num, 0, -1):
+#         f*=c
+#     return f
+# f1 = fatorial(5)
+# f2 = fatorial(4)
+# f3 = fatorial()
+# print(f'Os resultados são {f1}, {f2}, {f3}')
+
+#Exemplo return valor logico 
+def parOuImpar(n=0):
+    if n % 2 == 0:
