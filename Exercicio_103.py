036a0e19df76a5f71c623a68854a40de9f206dad
Andersonlimaz <andersonlimaz@outlook.com>
2023-07-27 11:03:43 -0300

    commit exercicio 103

--- Exercicio_103.py
+++ Exercicio_103.py
@@ -1,3 +1,19 @@
 #faça um programa que tenha uma função chamada ficha(), que rebe dois parametros opcionais:
 #o nome de um jogador e quantos gols ele marcou 
 #O programa deverá ser capaz de mostrar a ficha do jogador, mesmo que algum dado não tenha sido informado corretamente
+
+def ficha(jog='<Desconhecido>', gol= 0):
+    print(f'O Jogador {jog} fez {gol} gol(s) no campeonato')
+
+
+#programa principal 
+n = str(input("Nome do jogador: "))
+g = str(input("Numero de gols: "))
+if g.isnumeric():
+    g = int(g)
+else:
+    g = 0
+if n.strip() == '':
+    ficha(gol=g)
+else:
+    ficha(n, g)
