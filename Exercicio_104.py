ddaf8abfe2506147cc5d4fd39f5d5868272c4e12
Andersonlimaz <andersonlimaz@outlook.com>
2023-07-27 11:16:14 -0300

    commit exercicio 104

--- Exercicio_104.py
+++ Exercicio_104.py
@@ -1,3 +1,20 @@
 #Crie um programa que tenha a função leialnt(), que vai funcionar de forma semelhante á função input do Python,
 #Só que fazendo a validação para aceitar apenas um valor numérico. 
 #EX n = leialnt('Digite um n ')
+def leiaInt(msg):
+    ok = False
+    valor = 0
+    while True:
+        n = str(input(msg))
+        if n.isnumeric():
+            valor = int(n)
+            ok = True
+        else:
+            print('\033[0;31mErro! Digite um número inteiro válido.\033[m ')
+        if ok:
+            break
+    return valor
+
+#PRINCIPAL
+n = leiaInt('Digite um número:')
+print(f'Você acabou de digitar o número {n}')
