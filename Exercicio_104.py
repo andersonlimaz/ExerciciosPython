a7add3bb628a2d19b44cb51558d8f295a3a5d71a
Andersonlimaz <andersonlimaz@outlook.com>
2023-07-27 09:03:43 -0300

    exercicios completos

---
+++ Exercicio_104.py
@@ -0,0 +1,3 @@
+#Crie um programa que tenha a função leialnt(), que vai funcionar de forma semelhante á função input do Python,
+#Só que fazendo a validação para aceitar apenas um valor numérico. 
+#EX n = leialnt('Digite um n ')
---
+++ Exercicio_106.py
@@ -0,0 +1,4 @@
+#Faça um mini-sistema que utilize o interactive Help do Python. 
+#O usuario vai digitar o comando e o  manual vai aparecer. 
+#Quando o usuario digitar a palavra 'Fim', o programa se encerrará. 
+#obs: use cores. 
