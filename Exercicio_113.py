dd107ebff553330ed8d4ea30f6a90774fe530887
Andersonlimaz <andersonlimaz@outlook.com>
2023-07-31 13:00:14 -0300

    commit exercicios

---
+++ Exercicio_113.py
@@ -0,0 +1,4 @@
+'''
+Reescreva a função leiaInt() que fizemos no exercicio 104, incluindo agora a possibilidade da digitação de um número de tipo inválido. 
+aproveite e crie também uma função leiaFloat() com a mesma funcionalidade. 
+'''
---
+++ Exercicio_114.py
@@ -0,0 +1,3 @@
+'''
+Crie um codigo python que teste se site pudim esta acessivel pelo computador usado. 
+'''
