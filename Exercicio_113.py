2ddb0ea3cfc01601d9da362031b18aceae98964a
andersonlimaz <93233117+andersonlimaz@users.noreply.github.com>
2023-07-31 19:35:44 -0300

    ex113

--- Exercicio_113.py
+++ Exercicio_113.py
@@ -1,4 +1,20 @@
 '''
-Reescreva a função leiaInt() que fizemos no exercicio 104, incluindo agora a possibilidade da digitação de um número de tipo inválido. 
+Reescreva a função leiaInt() que fizemos no exercicio 104, 
+incluindo agora a possibilidade da digitação 
 aproveite e crie também uma função leiaFloat() com a mesma funcionalidade. 
 '''
+def leiaInt(msg):
+    while True:
+        try:
+            n = int(input(msg))
+        except (ValueError, TypeError):
+            print('\033[31mErro: por favor, digite um número inteiro válido.\033[m')
+            continue
+        except (KeyboardInterrupt):
+            print('Entrada de dados interrompida pelo usuário')
+        else:
+            return n
+
+#PRINCIPAL
+n = leiaInt('Digite um número:')
+print(f'Você acabou de digitar o número {n}')
