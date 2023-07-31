924622231f78c25d3004bd13b06a98629fae00f7
andersonlimaz <93233117+andersonlimaz@users.noreply.github.com>
2023-07-31 19:41:45 -0300

    ex 113 correção

--- Exercicio_113.py
+++ Exercicio_113.py
@@ -11,9 +11,25 @@ def leiaInt(msg):
             print('\033[31mErro: por favor, digite um número inteiro válido.\033[m')
             continue
         except (KeyboardInterrupt):
-            print('Entrada de dados interrompida pelo usuário')
+            print('\033[31mO usuário preferiu não digitar esse número.\033[m')
+            return 0
         else:
             return n
+
+
+def leiafloat(msg):
+    while True:
+        try:
+            n= float(input(msg))
+        except (ValueError, TypeError):
+            print('\033[31mErro: por favor, digite um número real válido.\033[m')
+            continue
+        except (KeyboardInterrupt):
+            print('\033[31mUsuario preferiu não digitar esse número.\033[m')
+            return 0
+        else:
+            return n
+
 
 #PRINCIPAL
 n = leiaInt('Digite um número:')
