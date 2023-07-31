6b98deb9e3f3df5efc2e2af53fa361c6da4779ca
andersonlimaz <93233117+andersonlimaz@users.noreply.github.com>
2023-07-31 20:08:10 -0300

    ex114

--- Exercicio_114.py
+++ Exercicio_114.py
@@ -1,3 +1,12 @@
 '''
 Crie um codigo python que teste se site pudim esta acessivel pelo computador usado. 
 '''
+import urllib
+import urllib.request
+
+try:
+    site = urllib.request.urlopen('http://www.pudim.com.br')
+except urllib.error.URLError:
+    print('O site pudim não está acessivel no momento')
+else:
+    print('Consegui acessar o site')
