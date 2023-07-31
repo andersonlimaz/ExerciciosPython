2fa7a14ec0db040ce4478ba67b0541c839f3e0d1
andersonlimaz <93233117+andersonlimaz@users.noreply.github.com>
2023-07-31 19:35:55 -0300

    ex113

--- Exercicio_115C.py
+++ Exercicio_115C.py
@@ -1,3 +1,9 @@
 '''
 
+Crie um pequeno sistema modularizado que permita cadastrar pessoas pelo seu nome e idade
+em um arquivo de texto simples. 
+
+O sistema só vai ter 2 opção cadastrar uma nova pessoa e listar todas as pessoas cadastradas. 
+part C
+
 '''
